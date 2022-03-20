e08d519390a1dfafa395a87f29424b766ac40ceb
Fernando Fernandes <fernandofernandesantos@gmail.com>
2022-03-20 13:30:11 +0100

    Bet version of install/uninstall commands.

--- cmake_install_scripts/post_install.sh
+++ cmake_install_scripts/post_install.sh
@@ -3,6 +3,8 @@ set -e
 
 VAR_LOG_DIR=${1}
 HOME_DIR=${2}
+SUDOERS_ADD=${3}
+
 RC_LOCAL_PATH=/etc/rc.local
 
 rad_path=/"${VAR_LOG_DIR}"/radiation-benchmarks
@@ -14,23 +16,15 @@ chmod ugo+w "${rad_path}"
 chmod ugo+w "${rad_path}"/log
 
 # Enable the telnet run reboot
-if grep -Fq "/sbin/halt, /sbin/reboot, /sbin/poweroff" /etc/sudoers
+if grep -Fq "${SUDOERS_ADD}" /etc/sudoers
 then
     # code if found
-    echo "No need to enable reboot for this user, already enabled"
+    echo "+ No need to enable reboot for this user, already enabled"
 else
     # code if not found
-    echo "%admin ALL=NOPASSWD: /sbin/halt, /sbin/reboot, /sbin/poweroff" >> /etc/sudoers
+    echo "+ Enabling reboot for this user"
+    echo "${SUDOERS_ADD}" >> /etc/sudoers
 fi
-
-# Creating /etc/rc.local
-cat > "${RC_LOCAL_PATH}" <<EOF
-#!/bin/bash
-
-sudo ${HOME_DIR}/atBoot.sh &
-
-exit 0
-EOF
 
 chmod 777 "${RC_LOCAL_PATH}"
 chmod 777 "${HOME_DIR}"/atBoot.sh
